41aaacc422c46262f30a8576bcac8325a1f3c970
Yochay <yochze@gmail.com>
2014-11-19 09:39:03 +0200

    totalWeight done

--- ex3/totalWeight.py
+++ ex3/totalWeight.py
@@ -5,18 +5,40 @@
 # DESCRIPTION:                                                                  
 # #############################################################     
 
+RING_WEIGHT  = -1
+MAX_WEIGHT   = 100
 total_weight = 0
 
 while True:
+    # Iterating while input from user is valid and while 
+    # user has not entered the break trigger (the ring).
+
+    # Receiving weight 
     weight = int(input("Insert weights one by one:"))
-    if weight == -1:
-        print("Total pack is " + str(total_weight))
+    if weight == RING_WEIGHT:
+        # Ring weight (-1) make the program stop the iteration
+        # of receiving input from user, and print the total
+        # packed weight.
+
+        print("The total packed weight is " + str(total_weight))
         break
+
     elif weight < 0:
-        print("Error smaller than 0")
-        break
-    elif (total_weight + weight) > 100:
+        # Weight input cannot be negative, if it is - print warning
+        # to user, and continue with iteration (next input)
+
+        print("Weights must be non-negative")
+        next
+
+    elif (total_weight + weight) > MAX_WEIGHT:
+        # If total weight and the last weight input from user
+        # has passed max weight (100), print a warning
+        # and end the program.
+
         print("Overweight! Gandalf will not approve")
         break
+
     else:
+        # If everything is OK, increment total weight with the user 
+        # new weight input.
         total_weight += weight
