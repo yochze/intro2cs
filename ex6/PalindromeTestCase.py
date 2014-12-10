9f6f1df8bdfee1d5f48db62755226c277837e54a
Yochay E <yochze@gmail.com>
2014-12-10 09:59:40 +0200

    Palindrome done

--- ex6/PalindromeTestCase.py
+++ ex6/PalindromeTestCase.py
@@ -1,7 +1,6 @@
 import unittest
 
 import palindrome
-# import balanced_brackets
 
 
 class PalindromeTestCase(unittest.TestCase):
@@ -18,6 +17,19 @@ class PalindromeTestCase(unittest.TestCase):
                         "returned false for aabbcbbaa")
         self.assertFalse(palindrome.is_palindrome_1("aabbcbbca"),
                          "returned true for aabbcbbca")
+        self.assertTrue(palindrome.is_palindrome_1("evilolive"),
+                        "returned false for evilolive")
+        self.assertFalse(palindrome.is_palindrome_1("evil olive"),
+                         "returned true for evil olive")
+        self.assertTrue(palindrome.is_palindrome_1("mirrorrim"),
+                        "returned false for mirrorrim")
+        self.assertFalse(palindrome.is_palindrome_1("mirror rim"),
+                         "returned true for mirror rim")
+        self.assertTrue(palindrome.is_palindrome_1("stackcats"),
+                        "returned false for stackcats")
+        self.assertFalse(palindrome.is_palindrome_1("stack cats"),
+                         "returned true for stack cats")
+
 
     def test_is_palindrome_2(self):
         self.assertTrue(palindrome.is_palindrome_2("141", 0, 2),
@@ -32,7 +44,27 @@ class PalindromeTestCase(unittest.TestCase):
                         "returned false for aabbcbbaa")
         self.assertFalse(palindrome.is_palindrome_2("aabbcbbca", 0, 8),
                          "returned true for aabbcbbca")
-
-                                                
+        self.assertTrue(palindrome.is_palindrome_2("evilolive", 0, 8),
+                        "returned false for evilolive")
+        self.assertFalse(palindrome.is_palindrome_2("evil olive", 0, 9),
+                         "returned true for evil olive")
+        self.assertTrue(palindrome.is_palindrome_2("mirrorrim", 0, 8),
+                        "returned false for mirrorrim")
+        self.assertFalse(palindrome.is_palindrome_2("mirror rim", 0, 9),
+                         "returned true for mirror rim")
+        self.assertTrue(palindrome.is_palindrome_2("stackcats", 0, 8),
+                        "returned false for stackcats")
+        self.assertFalse(palindrome.is_palindrome_2("stack cats", 0, 9),
+                         "returned true for stack cats")
+        self.assertTrue(palindrome.is_palindrome_2("bubbles", 0, 2),
+                        "returned false for bubbles, 0, 2")
+        self.assertFalse(palindrome.is_palindrome_2("bubbles", 0, 3),
+                         "returned true for bubbles, 0, 3")
+        self.assertTrue(palindrome.is_palindrome_2("bubbles", 2, 3),
+                        "returned false for bubbles, 2, 3")
+        self.assertFalse(palindrome.is_palindrome_2("bubbles", 1, 3),
+                         "returned true for bubbles, 1, 3")
+        self.assertTrue(palindrome.is_palindrome_2("143", 2, 1),
+                        "returned false for 143")
 if __name__ == '__main__':
     unittest.main()
--- ex6/palindrome.py
+++ ex6/palindrome.py
@@ -1,21 +1,66 @@
+#############################################################
+# FILE : palindrome.py
+# WRITER : Yochay Ettun , yochze
+# EXERCISE : intro2cs ex6
+# DESCRIPTION:
+# Two recursive functions to determine whether a string is palindrome
+# or not.
+# The first gets a full string and returns boolean according to the 
+# string palindromically.
+# The second program gets a string and two indexes representing a 
+# substring in the original string. Then it tests if the substring
+# is palindrome or not. return a boolean accordingly.
+#############################################################
+
+
 def is_palindrome_1(my_string):
-    if len(my_string) == 0:
+    """ Recursive program to check if a string is palindrome.
+        every iteration the program compare two values (first and last char)
+        if they match, it substract those chars and compare recursively
+        next two chars until it's the size of 1 or 0.
+        Then it returns True.
+
+        INPUT: Any string
+        OUTPUT: True if palindrome, otherwise False
+        """
+    if len(my_string) < 2: 
+        # Base condition, if we reached here then the string
+        # is palindrome. So return true
         return True
     else:
-        if my_string[:1] != my_string[-1:]:
+        # Length of string is not <2 , continue matching chars
+        # recursively.
+        if my_string[0] != my_string[-1]:
+            # No match between last and first char of current string.
+            # string is not palindrome. Return false.
             return False
         else:
-            is_palindrome_1(my_string[1:-1])
+            # String is still palindrome. Continue next iteration
+            # without first and last chars of current string.
+            return is_palindrome_1(my_string[1:-1])
 
 def is_palindrome_2(s, i, j):
+    """ Recursive program to determine if a substring is palindrome.
+        the program is similar to the previous program, but different
+        as it is given with two indexes to determine if the substring
+        of those indexes is palindrome.
+
+        INPUT: Any string, i and j integers in the range of len(string).
+        OUTPUT: True if palindrome, otherwise False
+    """
     if i > j or len(s) == 0:
+        # Base case:
+        # If smaller index (by definition) is bigger than larger index
+        # Or the length of the string is 0 , it means that the string
+        # has passed all iteration and is palindrome. return true.
         return True
     else:
         if s[i] == s[j]:
-            is_palindrome_2(s, i+1, j-1)
+            # Compare last and first char.
+            # If they're true, continue to the next pair of chars
+            # while incrementing i and decrementing j .
+            return is_palindrome_2(s, i+1, j-1)
         else:
+            # Chars are not matching, the string is not palindrome.
+            # Stop and return false.
             return False
-
-
-# print(is_palindrome_1(''))
-# print(is_palindrome_2('', 0,0))
