7c6de68821369ae79fc98bdc335fc1b3a491db02
Yochay E <yochze@gmail.com>
2014-12-16 12:58:04 +0200

    ex7 WIP

--- ex7/ex7.py
+++ ex7/ex7.py
@@ -21,8 +21,8 @@ def is_point_inside_triangle(point, v1, v2, v3):
     right_hand_lists = [point[0], point[1], 1]
 
     a, b, c = solve_linear_3(coefficients_lists, right_hand_lists)
-
-    if a < 0 or b < 0 or c < 0:
+    
+    if a <= 0 or b <= 0 or c <= 0:
         return (False, (a,b,c))
     else:
         return (True, (a,b,c))
@@ -38,10 +38,42 @@ def create_triangles(list_of_points):
 
 
     """
+    points = range(4, len(list_of_points))
+    triangles = []
+    
+    # Add the initial triangles (corners of the images)
+
+    triangles.append((list_of_points[0], list_of_points[1], list_of_points[2])) 
+    triangles.append((list_of_points[0], list_of_points[2], list_of_points[3])) 
+
+    for point_idx in range(4, len(list_of_points)):
+        p = list_of_points[point_idx]
+
+        for t in range(0, len(triangles)):
+            v1, v2, v3 = triangles[t]
+            if is_point_inside_triangle(p, v1, v2, v3)[0]:
+                new_triangles = add_3_triangles(p, v1, v2, v3)
+                triangles.pop(t)
+                triangles.extend(new_triangles)
+
+    return triangles
+
+def add_3_triangles(p, v1, v2, v3):
+    """
+    Helper function to add  3 triangles from 3 different points
+    and one anchor point (p).
+    """
+
+    t1 = (v1, v2, p)
+    t2 = (v2, v3, p)
+    t3 = (v3, v1, p)
+
+    return [t1, t2, t3]
+
 
 
 def do_triangle_lists_match(list_of_points1, list_of_points2):
-    pass
+    return True
 
 
 def get_point_in_segment(p1, p2, alpha):
