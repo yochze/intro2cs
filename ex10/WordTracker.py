2e44655f324303a0a707958951e2f81ee95656a0
Yochay E <yochze@gmail.com>
2015-01-15 10:13:30 +0200

    remove test prints

--- ex10/WordTracker.py
+++ ex10/WordTracker.py
@@ -62,8 +62,6 @@ class WordTracker(object):
         False otherwise.
         """
         if len(self._encountered_words) == self._list_size:
-            print("TRUEEEEz")
-            #print(self._encountered_words()):
             return True
         else:
             return False
